cdab284c054d65f27b8933e87837e52787004d2d
dimeng666-zq <861934989@qq.com>
2025-06-22 20:39:19 +0800

    添加 qiangFunc.c 和 qiangFunc.h 文件引用
    在 Project1.vcxproj 中添加了对 qiangFunc.c 和 qiangFunc.h 的引用。
    在 Project1.vcxproj.filters 中新增了相应的过滤器设置。
    在 main.c 中移除了原有的 print_current_date 函数实现，并调用了新的函数以输出当前日期和 "hello, world" 信息。

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 #include<stdio.h>
-#include <time.h>
-
-void print_current_date()
-{
-    time_t t = time(NULL);
-    struct tm* tm_info = localtime(&t);
-    printf("当前日期: %04d-%02d-%02d\n",
-        tm_info->tm_year + 1900,
-        tm_info->tm_mon + 1,
-        tm_info->tm_mday);
-}
+#include "qiangFunc.h"
 
 main()
 {
